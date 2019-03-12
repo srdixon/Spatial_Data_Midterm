4374eaf51b3b66c3d9bd5a5a558081dfe841a669
srdixon <sara.dixon1@louisiana.edu>
2019-03-12 13:41:24 -0500

    GitHub finally working- first commit

---
+++ midterm_data_process.R
@@ -0,0 +1,42 @@
+library(stringr)
+library(plyr)
+library(dplyr)
+library(lubridate)
+
+# Macrozooplankton data ----
+#read in  data
+z <- read.csv(file = "195101-201404_Zoop.csv", header = T)
+z <- z[,which(unlist(lapply(z, function(x)!all(is.na(x)))))] #using the "lapply" function from the "dplyr" package, remove fields which contain all "NA" values
+
+#create new fields with decimal degree latitude and longitude values
+z$Lat_DecDeg <- (z$Lat_Deg + (z$Lat_Min / 60))
+z$Lon_DecDeg <- (z$Lon_Deg + (z$Lon_Min / 60))
+
+# create a date-time field
+z$dateTime <- str_c(z$Tow_Date," ",z$Tow_Time,":00")
+z$dateTime <- as.POSIXct(strptime(z$dateTime, format = "%Y-%m-%d %H:%M:%S", tz = "America/Los_Angeles")) #Hint: look up input time formats for the 'strptime' function
+z$Tow_date <- NULL; z$Tow_time <- NULL
+
+#export data as tab delimited file
+write.csv(z, file = "zoop_clean.csv")
+
+#Egg data Set-----
+
+#read in data set
+e <- read.csv(file = "erdCalCOFIcufes_bb4a_5c83_ad3a.csv", header = T)
+
+#turn these character fields into date-time field
+e$stop_time_UTC <- gsub(x = e$stop_time_UTC, pattern = "T", replacement = " ")
+e$time_UTC <- gsub(x = e$time_UTC, pattern = "T", replacement = " ")
+e$time_UTC <-
+  
+  e <- e[,c(1:4,29,5:26)]
+
+#export data
+write.csv(e, file = "cufes_clean.csv")
+
+
+#merge
+zoop_cufes_merge = merge(e, z, by.x="cruise", by.y="Cruise")
+
+write.csv(zoop_cufes_merge, file = "zoop_cufes_total.csv")
